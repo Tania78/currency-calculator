08df000be6d61a1543a0d299a0c548b7f7a48d43
Tania78 <tanyanat25@gmail.com>
2020-10-28 14:57:24 +0000

    New image

---
+++ index.js
@@ -0,0 +1,35 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function caclulate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(`https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5`)
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      console.log('UAH to USD', data[0].sale);
+    });
+     
+}
+// Event listeners
+currencyEl_one.addEventListener('change', caclulate);
+amountEl_one.addEventListener('input', caclulate);
+currencyEl_two.addEventListener('change', caclulate);
+amountEl_two.addEventListener('input', caclulate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  caclulate();
+});
+
+caclulate();
